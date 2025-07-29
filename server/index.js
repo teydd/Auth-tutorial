bbb4c8d0cd47643861e16f8033acff415195839c
teydd <chemosted@gmail.com>
2025-07-29 23:08:02 +0300

    auth tutorial

---
+++ server/index.js
@@ -0,0 +1,21 @@
+const express = require("express")
+const mongoose = require("mongoose")
+require("dotenv").config()
+
+const auth = require("./routes/authRoute")
+
+PORT = process.env.PORT
+const app = express()
+
+app.use(express.json())
+app.use("/auth",auth)
+
+
+mongoose.connect(process.env.Test).then(()=>{
+    console.log("DB connected successfully")
+    app.listen(PORT, ()=>{
+        console.log("Server runing on port ", PORT)
+    })
+}).catch(()=>{
+    console.log("DB not connected")
+})
