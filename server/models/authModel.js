25faf134e1dbf432cc14ea022c6e4a6ba133ae22
teydd <chemosted@gmail.com>
2025-08-07 23:32:44 +0300

    auth model prettier

--- server/models/authModel.js
+++ server/models/authModel.js
@@ -1,33 +1,36 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 
-const authSchema = new mongoose.Schema({
-    email:{
-        type:String,
-        required:true,
+const authSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: true,
     },
-    password:{
-        type:String,
-        required:true
+    password: {
+      type: String,
+      required: true,
     },
-    name:{
-        type:String,
-        required:true
+    name: {
+      type: String,
+      required: true,
     },
-    isVerified:{
-        type:Boolean,
-        default:false
+    isVerified: {
+      type: Boolean,
+      default: false,
     },
-    role:{
-        type:String,
-        enum:["customer","admin"],
-        default:"customer"
+    role: {
+      type: String,
+      enum: ["customer", "admin"],
+      default: "customer",
     },
-    verificationToken:String,
-    verificationTokenExpiresAt:String,
-    resetPasswordToken:String,
-    resetPasswordTokenExpiresAt:String
-}, {timestamps:true})
+    verificationToken: String,
+    verificationTokenExpiresAt: String,
+    resetPasswordToken: String,
+    resetPasswordTokenExpiresAt: String,
+  },
+  { timestamps: true }
+);
 
-const User = mongoose.model("User",authSchema)
+const User = mongoose.model("User", authSchema);
 
-module.exports = User
+module.exports = User;
