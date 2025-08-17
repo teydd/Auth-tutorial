7ddfcfdcdfb6d21b6dbe70e65a3ccb7659963ea5
teydd <chemosted@gmail.com>
2025-08-17 23:07:08 +0300

    navbar

---
+++ client/auth/src/components/Nav.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+export default function Nav() {
+  return (
+    <>
+    <nav className="navbar navbar-expand-lg ">
+  <div className="container-fluid">
+    <a className="navbar-brand" href="/home">Navbar</a>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse align-items-center justify-content-center" id="navbarNavDropdown">
+      <ul className="navbar-nav">
+        <li className="nav-item">
+          <a className="nav-link active" aria-current="page" href="/home">Home</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="/dashboard">Dashboard</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+    </>
+  )
+}
