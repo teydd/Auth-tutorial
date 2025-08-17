e6f289aee3e3d1f4a6c5ad790ba6b6d3b6efd3d5
teydd <chemosted@gmail.com>
2025-08-17 23:27:44 +0300

    Form component

--- client/auth/src/pages/Dashboard.jsx
+++ client/auth/src/pages/Dashboard.jsx
@@ -2,6 +2,14 @@ import React from 'react'
 
 export default function Dashboard() {
   return (
-    <div>Dashboard</div>
+   <>
+   <div className='align-items-center justify-content-center d-flex mt-5 '>
+    <form className='form p-5 rounded-4'> 
+        <input className='form-control' type="text" name="" id="" /><br />
+        <input className='form-control' type="text" name="" id="" /><br />
+        <textarea className='form-control' name="" id=""></textarea>
+    </form>
+   </div>
+   </>
   )
 }
