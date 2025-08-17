767f230fe10471de3cda421de4349f36bb30b38c
teydd <chemosted@gmail.com>
2025-08-17 23:06:42 +0300

    app update

--- client/auth/src/App.jsx
+++ client/auth/src/App.jsx
@@ -1,4 +1,10 @@
 import { useState } from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import { BrowserRouter as Router, Routes, Route} from "react-router-dom"
+import Nav from './components/Nav'
+import Home from './pages/Home';
+import Dashboard from './pages/Dashboard';
 
 
 function App() {
@@ -6,6 +12,13 @@ function App() {
 
   return (
     <>
+    <Router>
+      <Nav></Nav>
+      <Routes>
+        <Route path='/home' element={<Home></Home>}></Route>
+        <Route path='/dashboard' element={<Dashboard></Dashboard>}></Route>    
+      </Routes>    
+    </Router>    
       
     </>
   )
