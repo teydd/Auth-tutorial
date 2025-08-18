cf7172fce7bd175372e41d4b9db6a2b682836f28
teydd <chemosted@gmail.com>
2025-08-18 23:28:46 +0300

    dashboard update

--- client/auth/src/pages/Dashboard.jsx
+++ client/auth/src/pages/Dashboard.jsx
@@ -3,15 +3,9 @@ import React from "react";
 export default function Dashboard() {
   return (
     <>
-      <div className="align-items-center justify-content-center d-flex mt-5 ">
-        <form className="form p-5 rounded-4">
-          <input className="form-control" type="email" name="" id="" />
-          <br />
-          <input className="form-control" type="text" name="" id="" />
-          <br />
-          <textarea className="form-control" name="" id=""></textarea>
-        </form>
-      </div>
+      <div className="container text-center p-5">
+      <p className="lead">Welcome to the Dashboard Section where all the summary of your information is stored</p>
+    </div>
     </>
   );
 }
