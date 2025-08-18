918ae19fd22dd3f0a2486e505ff8fbca6135ca3d
teydd <chemosted@gmail.com>
2025-08-18 23:21:11 +0300

    nav update

--- client/auth/src/components/Nav.jsx
+++ client/auth/src/components/Nav.jsx
@@ -1,26 +1,43 @@
-import React from 'react'
+import React from "react";
 
 export default function Nav() {
   return (
     <>
-    <nav className="navbar navbar-expand-lg ">
-  <div className="container-fluid">
-    <a className="navbar-brand" href="/home">Auth Tutorial</a>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
-    <div className="collapse navbar-collapse align-items-center justify-content-center" id="navbarNavDropdown">
-      <ul className="navbar-nav">
-        <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="/home">Home</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link active" href="/dashboard">Dashboard</a>
-        </li>
-      </ul>
-    </div>
-  </div>
-</nav>
+      <nav className="navbar navbar-expand-lg ">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="/home">
+            Auth Tutorial
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNavDropdown"
+            aria-controls="navbarNavDropdown"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div
+            className="collapse navbar-collapse align-items-center justify-content-center"
+            id="navbarNavDropdown"
+          >
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="/home">
+                  Home
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link active" href="/dashboard">
+                  Dashboard
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
     </>
-  )
+  );
 }
