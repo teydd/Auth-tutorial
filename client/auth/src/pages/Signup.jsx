2adc8055bc76e598b4aa2a65e6dbdd99467aacc8
teydd <chemosted@gmail.com>
2025-08-18 23:29:18 +0300

    signup component update

---
+++ client/auth/src/pages/Signup.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+export default function Signup() {
+  return (
+    <>
+    <div className="align-items-center justify-content-center d-flex mt-5 ">
+        <form className="form p-5 rounded-4">
+          <input className="form-control" type="email" name="" id="" placeholder="Email Address"/>
+          <br />
+          <input className="form-control" type="email" name="" id="" placeholder="Name"/>
+          <br />
+          <input className="form-control" type="email" name="" id="" placeholder="Phone number"/>
+          <br />
+          <input className="form-control" type="password" name="" id="" placeholder="Enter Password"/>
+          <br />
+          <button className='btn btn-lg btn-outline-success'>Submit</button>
+        </form>
+      </div>
+    </>
+  )
+}
