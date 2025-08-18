dd64306a9dbc92c23e9432a9ebdbf38d2b544058
teydd <chemosted@gmail.com>
2025-08-18 23:21:25 +0300

    dashboard update

--- client/auth/src/pages/Dashboard.jsx
+++ client/auth/src/pages/Dashboard.jsx
@@ -1,15 +1,17 @@
-import React from 'react'
+import React from "react";
 
 export default function Dashboard() {
   return (
-   <>
-   <div className='align-items-center justify-content-center d-flex mt-5 '>
-    <form className='form p-5 rounded-4'> 
-        <input className='form-control' type="text" name="" id="" /><br />
-        <input className='form-control' type="text" name="" id="" /><br />
-        <textarea className='form-control' name="" id=""></textarea>
-    </form>
-   </div>
-   </>
-  )
+    <>
+      <div className="align-items-center justify-content-center d-flex mt-5 ">
+        <form className="form p-5 rounded-4">
+          <input className="form-control" type="email" name="" id="" />
+          <br />
+          <input className="form-control" type="text" name="" id="" />
+          <br />
+          <textarea className="form-control" name="" id=""></textarea>
+        </form>
+      </div>
+    </>
+  );
 }
