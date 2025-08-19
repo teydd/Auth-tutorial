7b8951318b2b61a32644a6708b9053b69bbe7e95
teydd <chemosted@gmail.com>
2025-08-19 22:57:19 +0300

    reset update

--- client/auth/src/pages/PasswordReset.jsx
+++ client/auth/src/pages/PasswordReset.jsx
@@ -2,6 +2,16 @@ import React from 'react'
 
 export default function PasswordReset() {
   return (
-    <div>PasswordReset</div>
+    <div className="align-items-center justify-content-center d-flex mt-5 ">
+        <form className="form p-5 rounded-4">
+          <input className="form-control" type="email" name="" id="" placeholder="Email Address"/>
+          <br />
+          <input className="form-control" type="password" name="" id="" placeholder="Enter Password"/>
+          <br />
+          <input className="form-control" type="password" name="" id="" placeholder="Confirm Password"/>
+          <br />
+          <button className='btn btn-lg btn-outline-success'>Submit</button>
+        </form>
+      </div>
   )
 }
