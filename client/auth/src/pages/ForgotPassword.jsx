cc03e9d9a201a89ba359bcb8247600d158066421
teydd <chemosted@gmail.com>
2025-08-19 22:56:49 +0300

    forgot update

--- client/auth/src/pages/ForgotPassword.jsx
+++ client/auth/src/pages/ForgotPassword.jsx
@@ -2,6 +2,12 @@ import React from 'react'
 
 export default function ForgotPassword() {
   return (
-    <div>ForgotPassword</div>
+   <div className="align-items-center justify-content-center d-flex mt-5 ">
+        <form className="form p-5 rounded-4">
+          <input className="form-control" type="email" name="" id="" placeholder="Your Email Address"/>
+          <br />
+          <button className='btn btn-lg btn-outline-success'>Submit</button>
+        </form>
+      </div>
   )
 }
