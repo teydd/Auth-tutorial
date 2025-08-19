d7c4610a3c57913df003b718f05b9a6f32ec9d86
teydd <chemosted@gmail.com>
2025-08-19 22:57:34 +0300

    signin update

--- client/auth/src/pages/Signin.jsx
+++ client/auth/src/pages/Signin.jsx
@@ -3,6 +3,16 @@ import React from 'react'
 export default function Signin() {
   return (
     <>
+    <div className="align-items-center justify-content-center d-flex mt-5 ">
+        <form className="form p-5 rounded-4">
+          <input className="form-control" type="email" name="" id="" placeholder="Email Address"/>
+          <br />
+          <input className="form-control" type="password" name="" id="" placeholder="Enter Password"/>
+          <br />
+          <a href="/forgot-password"><i>Forgot Password?</i></a>
+          <button className='btn btn-lg btn-outline-success'>Submit</button>
+        </form>
+      </div>
     </>
   )
 }
