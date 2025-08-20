17cf0ae2bfd15d9d9f78cfeaba987bedeeaccf48
teydd <chemosted@gmail.com>
2025-08-20 21:47:48 +0300

    signin update

--- client/src/pages/Signin.jsx
+++ client/src/pages/Signin.jsx
@@ -7,10 +7,9 @@ export default function Signin() {
         <form className="form p-5 rounded-4">
           <input className="form-control" type="email" name="" id="" placeholder="Email Address"/>
           <br />
-          <input className="form-control" type="password" name="" id="" placeholder="Enter Password"/>
-          <br />
-          <a href="/forgot-password"><i>Forgot Password?</i></a>
-          <button className='btn btn-lg btn-outline-success'>Submit</button>
+          <input className="form-control mb-3" type="password" name="" id="" placeholder="Enter Password"/>
+          <a href="/forgot-password" className='nav-link'><i>Forgot Password?</i></a><hr />
+          <button className='btn  btn-outline-success active  w-100'>Submit</button>
         </form>
       </div>
     </>
