ace06453269a33d1bba124e59af14ba156aac6c7
teydd <chemosted@gmail.com>
2025-08-20 21:47:36 +0300

    forgot jsx update

--- client/src/pages/ForgotPassword.jsx
+++ client/src/pages/ForgotPassword.jsx
@@ -4,9 +4,10 @@ export default function ForgotPassword() {
   return (
    <div className="align-items-center justify-content-center d-flex mt-5 ">
         <form className="form p-5 rounded-4">
+          <label className='p-3' htmlFor="">Enter email associated with the account</label>
           <input className="form-control" type="email" name="" id="" placeholder="Your Email Address"/>
-          <br />
-          <button className='btn btn-lg btn-outline-success'>Submit</button>
+          <br /><hr />
+          <button className='btn btn-lg btn-outline-success active w-100'>Submit</button>
         </form>
       </div>
   )
