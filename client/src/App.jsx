8511189fb55c7f3466602278c014322aecb40e5d
teydd <chemosted@gmail.com>
2025-08-20 21:47:05 +0300

    route update

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,6 +6,7 @@ import Nav from "./components/Nav";
 import Home from "./pages/Home";
 import Dashboard from "./pages/Dashboard";
 import Signin from "./pages/Signin";
+import ForgotPassword from "./pages/ForgotPassword";
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
           <Route path="/home" element={<Home></Home>}></Route>
           <Route path="/dashboard" element={<Dashboard></Dashboard>}></Route>
           <Route path="/signin" element={<Signin></Signin>}></Route>
+          <Route path="/forgot-password" element={<ForgotPassword></ForgotPassword>}></Route>
         </Routes>
       </Router>
     </>
