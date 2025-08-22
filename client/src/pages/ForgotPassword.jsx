cc7bc613c326bd62025b98bdb908455cec2282a3
teydd <chemosted@gmail.com>
2025-08-22 20:34:49 +0300

    forgot prettier

--- client/src/pages/ForgotPassword.jsx
+++ client/src/pages/ForgotPassword.jsx
@@ -1,28 +1,44 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
 export default function ForgotPassword() {
-  const [form,setForm] = useState({email:""})
+  const [form, setForm] = useState({ email: "" });
 
-  const handleChange = (e)=>{
-    const {name,value} = e.target;
-    setForm((prev)=>({
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setForm((prev) => ({
       ...prev,
-      [name]:value
-    }))    
-  }
+      [name]: value,
+    }));
+  };
 
-  const handleSubmit = (e)=>{
-    e.preventDefault()
-    alert("Submitted successfully")
-  }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    alert("Submitted successfully");
+  };
   return (
-   <div className="align-items-center justify-content-center d-flex mt-5 ">
-        <form onSubmit={handleSubmit} className="form p-5 rounded-4">
-          <label className='p-3' htmlFor="">Enter email associated with the account</label>
-          <input onChange={handleChange} value={form.email} className="form-control" type="email" name="email" id="" placeholder="Your Email Address"/>
-          <br /><hr />
-          <button type='submit' className='btn btn-lg btn-outline-success active w-100'>Submit</button>
-        </form>
-      </div>
-  )
+    <div className="align-items-center justify-content-center d-flex mt-5 ">
+      <form onSubmit={handleSubmit} className="form p-5 rounded-4">
+        <label className="p-3" htmlFor="">
+          Enter email associated with the account
+        </label>
+        <input
+          onChange={handleChange}
+          value={form.email}
+          className="form-control"
+          type="email"
+          name="email"
+          id=""
+          placeholder="Your Email Address"
+        />
+        <br />
+        <hr />
+        <button
+          type="submit"
+          className="btn btn-lg btn-outline-success active w-100"
+        >
+          Submit
+        </button>
+      </form>
+    </div>
+  );
 }
