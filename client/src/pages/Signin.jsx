6192dbd746f33fcf7d888a8b265fbbd199745d70
teydd <chemosted@gmail.com>
2025-08-22 20:32:09 +0300

    sign in update

--- client/src/pages/Signin.jsx
+++ client/src/pages/Signin.jsx
@@ -1,16 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function Signin() {
+  const [form,setForm]= useState({
+    email:"",
+    password:""
+  })
+  const handleChange = (e)=>{
+    const {name, value} = e.target;
+    setForm((prev)=>({
+      ...prev,
+      [name]:value
+    }))
+  }
+  const handleSubmit = (e)=>{
+    e.preventDefault()
+    alert("Login successful")
+  }
   return (
     <>
     <div className="align-items-center justify-content-center d-flex mt-5 ">
-        <form className="form p-5 rounded-4">
+        <form onSubmit={handleSubmit} className="form p-5 rounded-4">
           <p className='text-center'>Sign in to your account</p><hr />
-          <input className="form-control" type="email" name="" id="" placeholder="Email Address"/>
+          <input onChange={handleChange} value={form.email} className="form-control" type="email" name="email" id="" placeholder="Email Address"/>
           <br />
-          <input className="form-control mb-3" type="password" name="" id="" placeholder="Enter Password"/>
+          <input onChange={handleChange} value={form.password} className="form-control mb-3" type="password" name="password" id="" placeholder="Enter Password"/>
           <a href="/forgot-password" className='nav-link'><i>Forgot Password?</i></a><hr />
-          <button className='btn  btn-outline-success active  w-100'>Sign in</button><hr />
+          <button type='submit' className='btn  btn-outline-success active  w-100'>Sign in</button><hr />
           <p>Don't have an account? <a href="/signup">Sign up</a></p>
         </form>
       </div>
