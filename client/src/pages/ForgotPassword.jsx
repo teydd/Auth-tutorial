51c339139ededa396f1a16fc5fa7e963eeb0e6bc
teydd <chemosted@gmail.com>
2025-08-22 20:31:41 +0300

    forgot update

--- client/src/pages/ForgotPassword.jsx
+++ client/src/pages/ForgotPassword.jsx
@@ -1,13 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function ForgotPassword() {
+  const [form,setForm] = useState({email:""})
+
+  const handleChange = (e)=>{
+    const {name,value} = e.target;
+    setForm((prev)=>({
+      ...prev,
+      [name]:value
+    }))    
+  }
+
+  const handleSubmit = (e)=>{
+    e.preventDefault()
+    alert("Submitted successfully")
+  }
   return (
    <div className="align-items-center justify-content-center d-flex mt-5 ">
-        <form className="form p-5 rounded-4">
+        <form onSubmit={handleSubmit} className="form p-5 rounded-4">
           <label className='p-3' htmlFor="">Enter email associated with the account</label>
-          <input className="form-control" type="email" name="" id="" placeholder="Your Email Address"/>
+          <input onChange={handleChange} value={form.email} className="form-control" type="email" name="email" id="" placeholder="Your Email Address"/>
           <br /><hr />
-          <button className='btn btn-lg btn-outline-success active w-100'>Submit</button>
+          <button type='submit' className='btn btn-lg btn-outline-success active w-100'>Submit</button>
         </form>
       </div>
   )
