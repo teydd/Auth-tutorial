3f0f9bf33374fdf318e5623ce3ea083fab4f748b
teydd <chemosted@gmail.com>
2025-08-23 23:21:42 +0300

    sign in update

--- client/src/pages/Signin.jsx
+++ client/src/pages/Signin.jsx
@@ -1,34 +1,62 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
 export default function Signin() {
-  const [form,setForm]= useState({
-    email:"",
-    password:""
-  })
-  const handleChange = (e)=>{
-    const {name, value} = e.target;
-    setForm((prev)=>({
+  const [form, setForm] = useState({
+    email: "",
+    password: "",
+  });
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setForm((prev) => ({
       ...prev,
-      [name]:value
-    }))
-  }
-  const handleSubmit = (e)=>{
-    e.preventDefault()
-    alert("Login successful")
-  }
+      [name]: value,
+    }));
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    alert("Login successful");
+  };
   return (
     <>
-    <div className="align-items-center justify-content-center d-flex mt-5 ">
+      <div className="align-items-center justify-content-center d-flex mt-5 ">
         <form onSubmit={handleSubmit} className="form p-5 rounded-4">
-          <p className='text-center'>Sign in to your account</p><hr />
-          <input onChange={handleChange} value={form.email} className="form-control" type="email" name="email" id="" placeholder="Email Address"/>
+          <p className="text-center">Sign in to your account</p>
+          <hr />
+          <input
+            onChange={handleChange}
+            value={form.email}
+            className="form-control"
+            type="email"
+            name="email"
+            id=""
+            placeholder="Email Address"
+          />
           <br />
-          <input onChange={handleChange} value={form.password} className="form-control mb-3" type="password" name="password" id="" placeholder="Enter Password"/>
-          <a href="/forgot-password" className='nav-link'><i>Forgot Password?</i></a><hr />
-          <button type='submit' className='btn  btn-outline-success active  w-100'>Sign in</button><hr />
-          <p>Don't have an account? <a href="/signup">Sign up</a></p>
+          <input
+            onChange={handleChange}
+            value={form.password}
+            className="form-control mb-3"
+            type="password"
+            name="password"
+            id=""
+            placeholder="Enter Password"
+          />
+          <a href="/forgot-password" className="nav-link">
+            <i>Forgot Password?</i>
+          </a>
+          <hr />
+          <button
+            type="submit"
+            className="btn  btn-outline-success active  w-100"
+          >
+            Sign in
+          </button>
+          <hr />
+          <p>
+            Don't have an account? <a href="/signup">Sign up</a>
+          </p>
         </form>
       </div>
     </>
-  )
+  );
 }
