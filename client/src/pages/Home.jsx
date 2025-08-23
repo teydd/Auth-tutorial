495e914861c816a4aada4203d735e08fd565cb0d
teydd <chemosted@gmail.com>
2025-08-23 23:20:38 +0300

    home prettier

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,11 +1,13 @@
-import React from 'react'
+import React from "react";
 
 export default function Home() {
   return (
     <>
-    <div className="container text-center p-5">
-      <p className="lead">Welcome to the Authentication Tutorial User interface</p>
-    </div>
+      <div className="container text-center p-5">
+        <p className="lead">
+          Welcome to the Authentication Tutorial User interface
+        </p>
+      </div>
     </>
-  )
+  );
 }
