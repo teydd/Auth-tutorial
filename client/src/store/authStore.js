b6f058efdc9176710a04cb29510a08acc4198237
teydd <chemosted@gmail.com>
2025-09-02 22:57:40 +0300

    auth store updated

--- client/src/store/authStore.js
+++ client/src/store/authStore.js
@@ -1,1 +1,10 @@
 import {create} from "zustand"
+import axios from "axios"
+
+export const useAuthStore = create((set)=>({
+    user:null,
+    isAuthenticated:false,
+    error:null,
+    isLoading:false,
+    isCheckingAuth:true
+}))
