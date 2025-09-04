620e7aceb60c0a85a53020aeb4ab16f793b33331
teydd <chemosted@gmail.com>
2025-09-04 23:47:42 +0300

    cors implementation

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const express = require("express")
 const mongoose = require("mongoose")
+const cors = require("cors")
 require("dotenv").config()
 
 const auth = require("./routes/authRoute")
@@ -8,7 +9,7 @@ const cookieParser = require("cookie-parser")
 
 PORT = process.env.PORT
 const app = express()
-
+app.use(cors({origin:"http://localhost:5173",credentials:true}))
 app.use(express.json())
 app.use(cookieParser())
 
