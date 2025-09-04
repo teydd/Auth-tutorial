efd1ea8a29b2af9d9ac3c74d18f06bf6c15f4339
teydd <chemosted@gmail.com>
2025-09-04 23:48:11 +0300

    prettier

--- server/index.js
+++ server/index.js
@@ -1,27 +1,28 @@
-const express = require("express")
-const mongoose = require("mongoose")
-const cors = require("cors")
-require("dotenv").config()
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv").config();
 
-const auth = require("./routes/authRoute")
+const auth = require("./routes/authRoute");
 
-const cookieParser = require("cookie-parser")
+const cookieParser = require("cookie-parser");
 
-PORT = process.env.PORT
-const app = express()
-app.use(cors({origin:"http://localhost:5173",credentials:true}))
-app.use(express.json())
-app.use(cookieParser())
+PORT = process.env.PORT;
+const app = express();
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(express.json());
+app.use(cookieParser());
 
+app.use("/auth", auth);
 
-app.use("/auth",auth)
-
-
-mongoose.connect(process.env.Test).then(()=>{
-    console.log("DB connected successfully")
-    app.listen(PORT, ()=>{
-        console.log("Server runing on port ", PORT)
-    })
-}).catch(()=>{
-    console.log("DB not connected")
-})
+mongoose
+  .connect(process.env.Test)
+  .then(() => {
+    console.log("DB connected successfully");
+    app.listen(PORT, () => {
+      console.log("Server runing on port ", PORT);
+    });
+  })
+  .catch(() => {
+    console.log("DB not connected");
+  });
