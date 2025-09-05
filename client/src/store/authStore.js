14b3c9990206fb6ad2a7cd0e291716c16601972b
teydd <chemosted@gmail.com>
2025-09-05 16:34:36 +0300

    verify function

--- client/src/store/authStore.js
+++ client/src/store/authStore.js
@@ -34,4 +34,16 @@ export const useAuthStore = create((set) => ({
       throw error;
     }
   },
+  verify: async(code)=>{
+    set({isLoading:true,error:null})
+
+    try {
+      const response = await axios.post(`${API_URL}/verify`, {code})
+      set({user:response.data.user, isAuthenticated:true,isLoading:false
+      })
+    } catch (error) {
+      set({error:error.response.data.message || "Error verifying email",isLoading:false}) 
+      throw error     
+    }
+  }
 }));
