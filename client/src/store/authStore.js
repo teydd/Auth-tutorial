cfa6603b93f13cd97a4fc18b7060ca22367c21cb
teydd <chemosted@gmail.com>
2025-09-14 23:38:27 +0300

    forgot functionality

--- client/src/store/authStore.js
+++ client/src/store/authStore.js
@@ -86,10 +86,17 @@ export const useAuthStore = create((set) => ({
   logout:{
 
   },
-  forgotPassword:{
-
+  forgotPassword:async(email)=>{
+    set({isLoading:true,error:null})
+    try {
+      const response = await axios.post(`${API_URL}/forgot-password`,{email})
+      set({message:response.data.message,isLoading:false})
+    } catch (error) {
+      set({isLoading:false})
+      throw error
+    }
   },
   resetPassword:{
-    
+
   }
 }));
